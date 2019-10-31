db3780ba6269d2d05e666c1aa0d924aa1575f019
gy15173348073 <gy@spacej.tech>
2019-10-31 13:45:03 +0800

    fdasfdas

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package TestGit
 import "fmt"
 
 func main() {
-	fmt.Println("+++++++++++++++++")
-	a := 1 + 1
-	c := a * 10 / 2
-	fmt.Println("c", c)
 	fmt.Println("----------------------")
+	a := 1 + 2
 	fmt.Println(a)
 }
